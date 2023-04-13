05d8f224775dc3cf7588cbd797cbc356cea6a295
jebinfrancis10k <129864949+jebinfrancis10k@users.noreply.github.com>
2023-04-13 17:36:03 +0530

    Add files via upload

---
+++ src/main/java/com/example/expensestracker/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.expensestracker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFD409F7)
+val Purple500 = Color(0xFF933AA3)
+val Purple700 = Color(0xFF5D1169)
+val Teal200 = Color(0xFFF44336)
